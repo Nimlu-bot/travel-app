6f7c7a498852256b307c7bb860b861b8617b5631
fiodar miculich <feotheodor@gmail.com>
2021-03-10 19:32:34 +0300

    feat: svg and search

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 export default function Language() {
     return (
         <div className='language-wrapper'>
-            <form>
-                <select size='1' required name='language[]'>
-                    <option value='EN'>EN</option>
-                    <option value='RU'>RU</option>
-                    <option value='BY'>BY</option>
-                </select>
-            </form>
+            <select size='1' required name='language[]'>
+                <option value='EN'>EN</option>
+                <option value='RU'>RU</option>
+                <option value='BY'>BY</option>
+            </select>
         </div>
     );
 }
--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default function SearchField() {
+
     return (
-        <div className='serch-wrapper'>
-            <input className='serch' autoComplete='off' placeholder='search' type='text' name='search' />
-            <div className='serch-button'>Search</div>
+        <div className='serch-box'>
+            <div className="toggle-search-button"></div>
+            <div  className='serch-wrapper'>
+                <input className='serch' autoComplete='off' placeholder='search' type='text' name='search' />
+                <div className='serch-button'></div>
+            </div>
         </div>
     );
 }
